e2f59ba6f4af4edb794d480457ad8a3f00721790
zhenekq <zhenek02ss@gmail.com>
2020-12-22 02:15:23 +0300

    created files

---
+++ src/main/java/com/zhenekns/Blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.zhenekns.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}

8ce610aa6a68a2bdd6a9f933b331ee48c8fe0a68
zhenekq <zhenek02ss@gmail.com>
2020-12-22 02:50:43 +0300

    inited a main page. Created BlogController

--- src/main/java/com/zhenekns/Blog/controllers/BlogController.java
+++ src/main/java/com/zhenekns/Blog/controllers/BlogController.java
@@ -5,12 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
-
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("title","Main page");
-        return "home";
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
     }
-
 }
